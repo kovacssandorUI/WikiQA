7795a773c59752fa9bc7d77a7a580f0576ccffd2
Sandor Kovacs <sandorkovacs122@gmail.com>
2018-01-02 00:40:29 +0100

    Add AuthenticatioinModel

---
+++ WikiQAApp/WikiQA.Data/AuthenticationModel/TokenRegistration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiQA.Data.AuthenticationModel
+{
+    class TokenRegistration
+    {
+        public int Userid { get; set; }
+        public string Token { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
