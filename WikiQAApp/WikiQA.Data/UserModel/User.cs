394b4906782bb1d3622af90539b7445076ad6d99
Sandor Kovacs <sandorkovacs122@gmail.com>
2018-01-02 00:36:51 +0100

    Add UserModel.

---
+++ WikiQAApp/WikiQA.Data/UserModel/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiQA.Data.UserModel
+{
+    class User
+    {
+        public int UserId { get; set; }
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int RoleId { get; set; }
+
+    }
+}
