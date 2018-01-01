b98f8b3cc4b9bfce3fee87d2e161fd5e912fb8c1
Sandor Kovacs <sandorkovacs122@gmail.com>
2018-01-02 00:42:50 +0100

    Add RoleModel

---
+++ WikiQAApp/WikiQA.Data/RoleModel/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiQA.Data.RoleModel
+{
+    class Role
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string RoleDescription { get; set; }
+    }
+}
