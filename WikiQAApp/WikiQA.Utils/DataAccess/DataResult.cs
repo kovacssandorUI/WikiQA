31ba9f48b9367557924e313caaa51e447a6a2bee
Sandor Kovacs <sandorkovacs122@gmail.com>
2018-01-02 20:22:33 +0100

    Add DataResult.

---
+++ WikiQAApp/WikiQA.Utils/DataAccess/DataResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiQA.Utils.DataAccess
+{
+    class DataResult : IEnumerable<Dictionary<string, object>>
+    {
+        List<Dictionary<string, object>> rows;
+        public DataResult()
+        {
+
+        }
+        public IEnumerator<Dictionary<string, object>> GetEnumerator()
+        {
+            return rows.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return rows.GetEnumerator();
+        }
+        public void AddRow(string[] fields, object[] values)
+        {
+            Dictionary<string, object> newrow = new Dictionary<string, object>();
+            for (int i = 0; i < fields.Length; i++)
+            {
+                newrow.Add(fields[i], values[i]);
+            }
+            rows.Add(newrow);
+        }
+    }
+}
