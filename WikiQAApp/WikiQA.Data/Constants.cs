56b6d26213b72ce1a2023a42acc44cd2a1217438
Sandor Kovacs <sandorkovacs122@gmail.com>
2018-01-02 11:05:37 +0100

    Add CONNECTIONSTRING.

---
+++ WikiQAApp/WikiQA.Data/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiQA.Data
+{
+    public class Constants
+    {
+        public const string CONNECTIONSTRING = @"Data Source=(LocalDb)\LocalDBDemo;Initial Catalog=WikiQA;Integrated Security=True";
+    }
+}
