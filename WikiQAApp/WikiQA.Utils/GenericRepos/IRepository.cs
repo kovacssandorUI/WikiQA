7d18e668094a6d6e62f933673c2e44e76120015f
Sandor Kovacs <sandorkovacs122@gmail.com>
2018-01-02 20:35:31 +0100

    Add IRepository.

---
+++ WikiQAApp/WikiQA.Utils/GenericRepos/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiQA.Utils.GenericRepos
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : class
+    {
+        Task<int> DeleteByIdAsync(int id);
+        Task<int> DeleteAsync(TEntity entity);
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IQueryable<TEntity>> GetAllAsync();
+        Task<IQueryable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> condition);
+        void Insert(TEntity entity);
+
+    }
+}
