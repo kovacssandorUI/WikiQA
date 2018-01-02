3896f5f63ca04436b66ec632d95fe1bec7451fe6
Sandor Kovacs <sandorkovacs122@gmail.com>
2018-01-02 18:54:52 +0100

    Add EventViewerLogger to WikiQA.Helper.

---
+++ WikiQAApp/WikiQA.Helper/Logger/EventViewerLogger.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiQA.Helper.Logger
+{
+    public static class EventViewerLogger
+    {
+        public static void LogError(string source, Exception e)
+        {
+            if (!EventLog.SourceExists(source))
+            {
+                EventLog.CreateEventSource(source, "WikiQA");
+            }
+            EventLog.WriteEntry(source, e.ToString(), EventLogEntryType.Error);
+        }
+
+        public static void LogError(string source, object objectToLog, Exception e)
+        {
+            var logObject = JsonConvert.SerializeObject(objectToLog);
+            if (!EventLog.SourceExists(source))
+            {
+                EventLog.CreateEventSource(source, "WikiQA");
+            }
+            EventLog.WriteEntry(source, e.ToString() + "\n" + logObject, EventLogEntryType.Error);
+        }
+
+        public static void LogInformation(string source, Exception e)
+        {
+            if (!EventLog.SourceExists(source))
+            {
+                EventLog.CreateEventSource(source, "WikiQA");
+            }
+            EventLog.WriteEntry(source, e.ToString(), EventLogEntryType.Information);
+        }
+
+        public static void LogInformation(string source, object objectToLog, string message = null)
+        {
+            var logObject = JsonConvert.SerializeObject(objectToLog);
+            if (!EventLog.SourceExists(source))
+            {
+                EventLog.CreateEventSource(source, "WikiQA");
+            }
+            if (message != null)
+            {
+                EventLog.WriteEntry(source, message + "\n" + logObject, EventLogEntryType.Information);
+            }
+            else
+            {
+                EventLog.WriteEntry(source, logObject, EventLogEntryType.Information);
+            }
+        }
+
+
+
+        public static void LogWarning(string source, Exception e)
+        {
+            if (!EventLog.SourceExists(source))
+            {
+                EventLog.CreateEventSource(source, "WikiQA");
+            }
+            EventLog.WriteEntry(source, e.ToString(), EventLogEntryType.Warning);
+        }
+    }
+
+
+}
